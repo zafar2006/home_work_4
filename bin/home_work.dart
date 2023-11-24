a6f1901cb347377fc2de1d4bdc7c6e52ebf0227b
zafar2006 <kasimovzafar683@gmail.com>
2023-11-24 16:30:51 +0600

    first commit

---
+++ bin/home_work.dart
@@ -0,0 +1,5 @@
+import 'package:home_work/home_work.dart' as home_work;
+
+void main(List<String> arguments) {
+  print('Hello world: ${home_work.calculate()}!');
+}
---
+++ test/home_work_test.dart
@@ -0,0 +1,8 @@
+import 'package:home_work/home_work.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
