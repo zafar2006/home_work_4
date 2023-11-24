cc987be421198ea04bacf5a26b745e2b0c2ac75d
zafar2006 <kasimovzafar683@gmail.com>
2023-11-24 16:34:55 +0600

    second commit

--- bin/home_work.dart
+++ bin/home_work.dart
@@ -1,5 +1,62 @@
-import 'package:home_work/home_work.dart' as home_work;
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world: ${home_work.calculate()}!');
+void main() {
+//Задача 1
+  // String a = 'kasimovzafar683@gmail.com';
+  // String g;
+  // do {
+  //   print('Введите логин');
+  //   g = stdin.readLineSync()!;
+  //   if (g == a) {
+  //     print('Верный логин:$a');
+  //   } else {
+  //     print('Неверный логин"$g');
+  //   }
+  // } while (g != a);
+
+  // int s = 331113;
+  // int d;
+  // do {
+  //   print('Введите пароль');
+  //   d = int.parse(stdin.readLineSync()!);
+  //   if (d == s) {
+  //     print('Верный пароль:$s');
+  //   } else {
+  //     print('Повторите ввод:$d');
+  //   }
+  // } while (d != s);
+
+// Задача 2
+  // int number = 0;
+  // List<int> numbers = [];
+  // int summ = 0;
+  // do {
+  //   print('Введите число ');
+  //   number = int.parse(stdin.readLineSync()!);
+  //   numbers.add(number);
+  // } while (number != 0);
+  // for (var i in numbers) {
+  //   summ += i;
+  // }
+  // print('Кол-во ${numbers.length - 1}');
+  // print('Сумма $summ');
+  // print('Среднее ${summ / (numbers.length - 1)}');
+
+// Задача 3
+  // print('Введите начало диапазона: ');
+  // int start = int.parse(stdin.readLineSync()!);
+
+  // print('Введите конец диапазона: ');
+  // int end = int.parse(stdin.readLineSync()!);
+
+  // int sum = 0;
+  // int a = start;
+
+  // do {
+  //   if (a % 2 != 0) {
+  //     sum += a;
+  //   }
+  //   a++;
+  // } while (a <= end);
+  // print('Сумма нечетных чисел в диапазоне от $start до $end: $sum');
 }
